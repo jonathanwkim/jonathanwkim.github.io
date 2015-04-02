598e5b21427d09c4311ad0dd681074c75a3b4334
saasbook <saasbook@saasbook.(none)>
2015-04-02 15:54:12 -0300

    changed names

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn site:app 
+web: gunicorn -w 2 -b 127.0.0.1:5000 startup:app
---
+++ startup.py
@@ -0,0 +1,39 @@
+import os
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from contextlib import closing
+import sqlite3
+
+# configuration
+DATABASE = '/tmp/personal_site.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('SITE_SETTINGS', silent=True)
+
+@app.route('/')
+def site():
+    return redirect(url_for('about'))
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+	return render_template('projects.html')
+
+@app.route('/resume')
+def resume():
+	return render_template('resume.html')
+
+@app.route('/blog')
+def blog():
+	return render_template('blog.html')
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
